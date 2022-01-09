909cbc0bd306278933188d5473afb414f601ae15
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-01-09 22:07:59 +0700

    created passport and session

---
+++ config/passport-config.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session')
+const bcrypt = require('bcrypt')
+
+const dbConnection = require("../config/dbConnect");
+
+const customFields = {
+  usernameField: 'email',
+  passwordField: 'password'
+};
+
+const verifyCallback = async (username, password, done) => {
+  let connection = await dbConnection();
+  connection.query("SELECT email,password FROM election_db.users WHERE email = ?", username, async (err, result)=> {
+    if(err){
+      done(err);
+    }else{
+      // console.log(result);
+      if(result.length == 0){
+        return done(null, false, { message: 'Invalid username or password' });
+      }else{
+        const match = await bcrypt.compare(password, result[0].password);
+        if(match){
+          return done(null, result);
+        }else{
+          return done(null, false, { message: 'Invalid username or password' });
+        }
+      }
+    }
+  });
+
+};
+
+const strategy  = new LocalStrategy(customFields, verifyCallback);
+
+passport.use(strategy);
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
